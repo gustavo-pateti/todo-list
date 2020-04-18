ebbf2a87e36e6c33acdb9bc9170409e66827c475
Gustavo Pateti <gpateti@gmail.com>
2020-04-17 23:13:41 -0400

    Updates

---
+++ precache-manifest.74f4e4056285166abfde9349bc705bd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56aaafbfc915c1352de4bf11fb8a7e07",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "0c1e12c48ded2e4d4043",
+    "url": "/todo-list/static/css/main.9b87fd7b.chunk.css"
+  },
+  {
+    "revision": "5d1ba80fae11e036e071",
+    "url": "/todo-list/static/js/2.ab0bb51d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.ab0bb51d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c1e12c48ded2e4d4043",
+    "url": "/todo-list/static/js/main.b3c8ab3b.chunk.js"
+  },
+  {
+    "revision": "4c207b602d40809001a5",
+    "url": "/todo-list/static/js/runtime-main.2929926a.js"
+  }
+]);
