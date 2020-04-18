70eecc5383cf3a19fae4ef99e4a68e253ce7098e
Gustavo Pateti <gpateti@gmail.com>
2020-04-17 22:09:08 -0400

    Add checkboxes to items

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,7 +2,16 @@ import React, { Component } from "react";
 
 export class TodoItem extends Component {
   render() {
-    return <li>{this.props.todo.title}</li>;
+    return (
+      <li>
+        <input
+          type="checkbox"
+          checked={this.props.todo.completed}
+          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+        />
+        {this.props.todo.title}
+      </li>
+    );
   }
 }
 
